669e204893be2fca63d88a757ad77e4ae81ffc16
Rahul <rahulyiet@gmail.com>
2018-06-27 07:11:24 -0700

    first commit

---
+++ app/src/main/java/com/example/rahulyiet/newpermission/Main2Activity.java
@@ -0,0 +1,50 @@
+package com.example.rahulyiet.newpermission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    EditText OTP;
+    Button Submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        OTP = findViewById(R.id.OTP);
+        Submit = findViewById(R.id.Submit);
+
+        String  message = getIntent().getStringExtra("message");
+
+        final int x = Integer.valueOf(message);
+
+        Log.d("test"," "+x);
+
+        Toast.makeText(this," " + x , Toast.LENGTH_LONG ).show();
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String UserOTP = OTP.getText().toString().trim();
+                Integer y = Integer.valueOf(UserOTP);
+
+                if (x == y) {
+                    Toast.makeText(Main2Activity.this, "Submitted Successfully", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(Main2Activity.this, "Wrong Otp", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/rahulyiet/newpermission/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.rahulyiet.newpermission;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+      private   EditText Phone,Message;
+         private Button Send;
+
+         private static final int REQUEST_CODE_SMS =101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Phone =findViewById(R.id.phone);
+        Message =findViewById(R.id.message);
+        Send=findViewById(R.id.button);
+
+        String[] permission = {
+                Manifest.permission.SEND_SMS
+
+        };
+
+           if(ActivityCompat.checkSelfPermission(MainActivity.this,permission[0]) != PackageManager.PERMISSION_GRANTED){
+
+               ActivityCompat.requestPermissions(MainActivity.this,
+               permission,123);
+           }
+
+              Send.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View view) {
+
+                      String phoneNo = Phone.getText().toString().trim();
+                      String message = Message.getText().toString().trim();
+
+                      SmsManager sms =SmsManager.getDefault();
+                      Random r =new Random();
+                      int i1 =r.nextInt(9999 - 1111) + 1111;
+                      String MessageNumber=i1+"";
+
+                      sms.sendTextMessage(phoneNo,null,MessageNumber,null,null);
+
+                      Toast.makeText(MainActivity.this, MessageNumber , Toast.LENGTH_LONG).show();
+
+                      Intent intent =new Intent(MainActivity.this,Main2Activity.class);
+                      intent.putExtra("message",MessageNumber);
+                      startActivity(intent);
+
+                  }
+              });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==123){
+            if (grantResults.length>0 &&
+                    grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                Toast.makeText(MainActivity.this,"Permission Granted !",Toast.LENGTH_SHORT).show();
+            }else{
+
+                Toast.makeText(MainActivity.this,"Permission not granted",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+}
